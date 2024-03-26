21c2bac0dd55743edede5fefe7bcdc7555d2f4c5
j-Karnika <jayanthkarnika@gmail.com>
2024-03-26 16:14:07 +0530

    first commit, working fine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+import Results from './Results';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState('');
+
+  const token = process.env.REACT_APP_AUTH_TOKEN;
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const url = 'https://discoveryengine.googleapis.com/v1alpha/projects/700470856861/locations/global/collections/default_collection/dataStores/jk-data-store-new_1710913057488/servingConfigs/default_search:search';
+
+      const body = JSON.stringify({
+        query: query,
+        pageSize: 10,
+        queryExpansionSpec: {
+          condition: 'AUTO',
+        },
+        spellCorrectionSpec: {
+          mode: 'AUTO',
+        },
+        contentSearchSpec: {
+          summarySpec: {
+            summaryResultCount: 5,
+            modelSpec: {
+              version: 'stable',
+            },
+            ignoreAdversarialQuery: true,
+            includeCitations: true,
+          },
+          snippetSpec: {
+            maxSnippetCount: 1,
+            returnSnippet: true,
+          },
+          extractiveContentSpec: {
+            maxExtractiveAnswerCount: 1,
+          },
+        },
+      });
+
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body,
+      });
+
+      if (!response.ok) {
+        throw new Error('Error fetching results. Please try again.');
+      }
+
+      const data = await response.json();
+      setResults(data);
+      setError('');
+    } catch (err) {
+      setError(err.message);
+      console.error(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Google Cloud Datastore Search</h1>
+      <div className="input-container">
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Enter your query..."
+        className="input"
+      />
+      <button onClick={handleSubmit} className="button">
+        Send
+      </button>
+      </div>
+      {error && <p className="error">{error}</p>}
+      <Results documents={results.results} summary={results.summary}/>
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,75 @@
+
+import React, { useState } from 'react';
+import "./results.css"
+
+const Results = ({ documents, summary }) => {
+  const [selectedConversationStyle, setSelectedConversationStyle] = useState('');
+
+  const handleConversationStyleChange = (event) => {
+    setSelectedConversationStyle(event.target.value);
+    console.log(selectedConversationStyle)
+  };
+
+  console.log("this is the documents", documents)
+
+  const filteredDocuments = selectedConversationStyle ?
+    documents.filter((document) => {
+      const structData = document.document.structData;
+      return structData && structData.conversation_style === selectedConversationStyle;
+    }) :
+    documents;
+
+  if (!Array.isArray(documents) || documents.length === 0) {
+    return <div>No documents to display</div>;
+  }
+
+  return (
+    <div className="response-container">
+      <div className="all-filters-container">
+        <div className="filters">
+          <label htmlFor="conversationStyle">Conversation Style:</label>
+          <select
+            id="conversationStyle"
+            value={selectedConversationStyle}
+            onChange={handleConversationStyleChange}
+            className="dropdown"
+          >
+            <option value="" defaultValue="">All</option>
+            <option value="board">Board</option>
+            <option value="forum">Forum</option>
+            <option value="tkb">TKB</option>
+          </select>
+        </div>
+      </div>
+      <div className="results">
+        {filteredDocuments.map((document) => (
+          <div key={document.id} className="document">
+            <p>
+              <strong>Document ID:</strong> {document.id}
+            </p>
+            <p>
+              <strong>Generative response:</strong> {summary.summaryText}
+            </p>
+            <p className="content">
+              <strong>Content:</strong> {document.document.derivedStructData.extractive_answers[0].content}
+            </p>
+            <p>
+              <strong>Title:</strong> {document.document.derivedStructData.title}
+            </p>
+            <p>
+              <strong>Google cloud file location:</strong>{' '}
+              <span className="path">{document.document.derivedStructData.link}</span>
+            </p>
+            {document.document.structData && (
+              <p>
+                <strong>Author:</strong> {document.document.structData.author}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Results;
